219b388d3545d511e4fbbd946a39704704d73fe9
Alexander Korzh <alexanderkorzh@MacBook-Air-Alexander.local>
2023-08-28 21:28:38 +0300

    commented tests

--- tests/registerLoginErr.test.js
+++ tests/registerLoginErr.test.js
@@ -1,88 +1,88 @@
-const registerLogin = require("../registerLoginErr")
-const dbOperationsUser = require("../dbOperations/dbOperationsUser")
-describe("CheckRegister function", () => {
-    it("Passed test", async () => {
-        const {isPassed, errText} = await registerLogin.checkRegister("1234", "1234", "Splay1222@gmail.com");
+// const registerLogin = require("../registerLoginErr")
+// const dbOperationsUser = require("../dbOperations/dbOperationsUser")
+// describe("CheckRegister function", () => {
+//     it("Passed test", async () => {
+//         const {isPassed, errText} = await registerLogin.checkRegister("1234", "1234", "Splay1222@gmail.com");
   
-        console.log(errText)
+//         console.log(errText)
 
-        expect(isPassed).toBe(true);
-        expect(errText.length).toBe(0);
-    });
+//         expect(isPassed).toBe(true);
+//         expect(errText.length).toBe(0);
+//     });
   
   
   
-    it("Not passed",async () => {
-      let {isPassed,errText} = await registerLogin.checkRegister("123","1234","Splay1494@gmail.com")
+//     it("Not passed",async () => {
+//       let {isPassed,errText} = await registerLogin.checkRegister("123","1234","Splay1494@gmail.com")
   
-      expect(isPassed).toBe(false)
-      expect(errText[0]).toBe("Passwords are not the same")
-    })
+//       expect(isPassed).toBe(false)
+//       expect(errText[0]).toBe("Passwords are not the same")
+//     })
   
-    it("email err",async () => {
-      let {isPassed,errText} = await registerLogin.checkRegister("1234","1234","Splay1494")
+//     it("email err",async () => {
+//       let {isPassed,errText} = await registerLogin.checkRegister("1234","1234","Splay1494")
       
-      expect(isPassed).toBe(false)
-      expect(errText[0]).toBe("Not valid email")
+//       expect(isPassed).toBe(false)
+//       expect(errText[0]).toBe("Not valid email")
   
-    })
+//     })
 
-    // it("email already used",async () => {
-    //   let {isPassed,errText} = await registerLogin.checkRegister("1234","1234","Splay1494@gmail.com")
+//     it("email already used",async () => {
+//       let {isPassed,errText} = await registerLogin.checkRegister("1234","1234","Splay1494@gmail.com")
 
-    //   console.log(errText)
-    //   expect(isPassed).toBe(false)
-    //   expect(errText[0]).toBe("Email already used")
-    // })
+//       console.log(errText)
+//       expect(isPassed).toBe(false)
+//       expect(errText[0]).toBe("Email already used")
+//     })
   
-    it("All errs",async () => {
-      let {isPassed,errText} = await registerLogin.checkRegister("1234","123","Splay1494")
+//     it("All errs",async () => {
+//       let {isPassed,errText} = await registerLogin.checkRegister("1234","123","Splay1494")
       
-      expect(isPassed).toBe(false)
-      expect(errText.join(" ")).toBe("Passwords are not the same Not valid email")
+//       expect(isPassed).toBe(false)
+//       expect(errText.join(" ")).toBe("Passwords are not the same Not valid email")
   
-    })
+//     })
 
-    it("Get user id",async () => {
-      let username = "Splay"
-      let email = "Splay1494@gmail.com"
-      const res = await registerLogin.getId(username,email)
+//     it("Get user id",async () => {
+//       let username = "Splay"
+//       let email = "Splay1494@gmail.com"
+//       const res = await registerLogin.getId(username,email)
 
-      expect(res).toBe(103)
-    })
+//       expect(res).toBe(103)
+//     })
 
 
-  });
+//   });
 
-describe("checkLogin",() => {
-  // it("LoginCheck",async () => {
-  //   let {isPassed,errText} = await registerLogin.checkLogin("Splay1494@gmail.com","Splay1494")
+// describe("checkLogin",() => {
+//   // it("LoginCheck",async () => {
+//   //   let {isPassed,errText} = await registerLogin.checkLogin("Splay1494@gmail.com","Splay1494")
 
-  //   expect(isPassed).toBe(true)
-  //   expect(errText.length).toBe(0)
-  // })
+//   //   expect(isPassed).toBe(true)
+//   //   expect(errText.length).toBe(0)
+//   // })
 
-  it("Email err",async () => {
-    let {isPassed,errText} = await registerLogin.checkLogin("Splay2007@gmail.com","Splay1494")
-    expect(isPassed).toBe(false)
-    expect(errText[0]).toBe("Email or password is wrong")
-  })
+//   it("Email err",async () => {
+//     let {isPassed,errText} = await registerLogin.checkLogin("Splay2007@gmail.com","Splay1494")
+//     expect(isPassed).toBe(false)
+//     expect(errText[0]).toBe("Email or password is wrong")
+//   })
 
-  it("Password err",async () => {
-    let {isPassed,errText} = await registerLogin.checkLogin("Splay1494@gmail.com","Splay1")
-    expect(isPassed).toBe(false)
-    expect(errText[0]).toBe("Email or password is wrong")
-  })
+//   it("Password err",async () => {
+//     let {isPassed,errText} = await registerLogin.checkLogin("Splay1494@gmail.com","Splay1")
+//     expect(isPassed).toBe(false)
+//     expect(errText[0]).toBe("Email or password is wrong")
+//   })
 
-  it("All err",async () => {
-    let {isPassed,errText} = await registerLogin.checkLogin("Splay1494@gmail","Splay1")
-    expect(isPassed).toBe(false)
-    expect(errText[0]).toBe("Email or password is wrong")
-  })
+//   it("All err",async () => {
+//     let {isPassed,errText} = await registerLogin.checkLogin("Splay1494@gmail","Splay1")
+//     expect(isPassed).toBe(false)
+//     expect(errText[0]).toBe("Email or password is wrong")
+//   })
 
-  // it("helper function for login",async () => {
-  //   let email = await dbOperationsUser.getUsername("Splay1494@gmail.com")
+//   it("helper function for login",async () => {
+//     let email = await dbOperationsUser.getUsername("Splay1494@gmail.com")
 
-  //   expect(email).toBe("Splay")
-  // })
-})
+//     expect(email).toBe("Splay")
+//   })
+// })
